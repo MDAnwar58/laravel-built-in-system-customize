bb953bfc96069221f6e2f81d5a669cfea0470882
MDAnwar58 <anwar.saeed656@gmail.com>
2025-12-24 08:54:18 +0600

    if you are customize built-in authenticate middlewre and with role based middleware

---
+++ app/Http/Middleware/Authenticate.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class Authenticate
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, ...$guards): Response
+    {
+        $guards = empty($guards) ? [null] : $guards;
+
+        foreach ($guards as $guard) {
+            if (Auth::guard($guard)->check()) {
+                $request->user = Auth::guard($guard)->user();
+                return $next($request);
+            }
+        }
+
+        // If the user is not authenticated, redirect to login
+        if ($request->expectsJson()) {
+            return response()->json(['message' => 'Unauthenticated.'], 401);
+        }
+
+        return redirect()->guest(route('login'));
+    }
+}
